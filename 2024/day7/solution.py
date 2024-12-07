9dd683dd1dd8090318bbe4671e4676a9b9f40815
Tae Min Ha <taemin410@gmail.com>
2024-12-07 15:28:36 +0800

    brute force day7

---
+++ 2024/day7/solution.py
@@ -0,0 +1,50 @@
+
+import numpy as np
+
+
+f = open("day7.input", "r+")
+
+lines = [ a.strip() for a in f.readlines() ] 
+
+def operation_concat(a, b):
+    base10 = len(str(b))
+    res = a * (10 ** base10) + b 
+    return res
+    
+
+sum = 0
+for line in lines:
+    
+    numbers = line.split(":")
+    result = int(numbers[0])
+
+    operands = [ int(i) for i in numbers[1].strip().split(" ")] 
+
+
+    queue = []
+
+    x = 0
+    queue.append((operands[x], x))
+    while len(queue) > 0: 
+        
+        ans, pos = queue.pop(0)
+        if pos + 1 == len(operands):
+            #print(ans)
+            if ans == result:
+                sum += ans
+                break
+        elif ans <= result:
+            # print(ans, operands[pos])
+            queue.append((ans + operands[pos+1], pos+1))
+            queue.append((ans * operands[pos+1], pos+1))
+            queue.append((operation_concat(ans,operands[pos+1]), pos+1))
+
+
+print(sum)
+    
+
+    
+
+
+
+
